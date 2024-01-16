c592d2a049ac117f920566660a57e00509740df2
Joshua Müller <joshua.mueller2005@web.de>
2024-01-16 15:42:07 +0100

    Added PHP and Syle.css

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TheButton</title>
+    <link rel="style.css" href="style.css">
 </head>
 <body>
-    <input type="button" name="button" value="Press Me">
+    <?php
+    echo('<input type="button" name="button" value="Press Me">');    
+    ?>
 </body>
 </html>
