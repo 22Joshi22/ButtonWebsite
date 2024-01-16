f6f366e286cccc71635544dcf9b6e2a8ba4188ec
YouPromisedMeFreeRobux <florian.dpiering@gmail.com>
2024-01-16 16:53:15 +0100

    Changed Funktion

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TheButton</title>
     <link rel="icon" type="image/x-icon" href="byuntear-sad.ico">
 </head>
+
 <body>
     <style>
         <?php include "style.css"; ?>
@@ -14,31 +16,32 @@
 
     <!--Scripts-->
     <script>
-        function showLeaderboard() {
+        let open = false;
+        function ToggleLeaderboard() {
             var modalLeaderboard = document.getElementById("modalLeaderboard");
-            modalLeaderboard.style.display= "block";
-        }
-
-        function closeLeaderboard() {
-            var modalLeaderboard = document.getElementById("modalLeaderboard");
-            modalLeaderboard.style.display= "none";
+            if (open) {
+                modalLeaderboard.style.display = "none";
+            } else {
+                modalLeaderboard.style.display = "block";
+            }
         }
     </script>
     <?php
     //echo the button
     //Main Button
-    echo('<input class="button" type="button" name="button" value="Press Me">'); 
+    echo ('<input class="button" type="button" name="button" value="Press Me">');
 
     //Leaderboard
-    echo('<input type="button" name="leaderboard" value="Leaderboard" onclick="showLeaderboard()">'); 
+    echo ('<input type="button" name="leaderboard" value="Leaderboard" onclick="showLeaderboard()">');
     //Set variables
-    $currentpoints=0;
-    $maxpoints=0;
-    $resets=0;
+    $currentpoints = 0;
+    $maxpoints = 0;
+    $resets = 0;
     //set cookies
-    setcookie("currentpoints", $currentpoints); 
-    setcookie("maxpoints", $maxpoints);   
+    setcookie("currentpoints", $currentpoints);
+    setcookie("maxpoints", $maxpoints);
     setcookie("resets", $resets);
     ?>
 </body>
+
 </html>
